4973775e5581185842bbd898555452570d7501c7
ArenPetrossian <57547520+ArenPetrossian@users.noreply.github.com>
2019-12-09 14:07:23 -0800

    Add files via upload

---
+++ Magikarp_Code/Horizontal_Thruster_Testing.ino
@@ -0,0 +1,138 @@
+/* Robosub 2019-2020 Horizontal Thruster Testing
+   
+   Current Revision 11 14 19
+   Feature: IMU for X angle only/ Output for Horizontal only
+   
+   Aren Petrossian
+*/
+
+
+
+//All librarys and IMU setup
+#include <PID_v1.h>
+#include <Wire.h>
+#include <Servo.h>
+#include <Adafruit_BNO055.h>
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+Adafruit_BNO055 bno = Adafruit_BNO055();
+
+
+
+//Start thrusters as servos and set Pins**
+Servo left_thruster;
+Servo right_thruster;
+double final_left_thruster_value;
+double final_right_thruster_value;
+int left_thruster_pin = 11;         //****
+int right_thruster_pin = 10;        //****
+
+
+
+//Start all PIDs and set k values**
+double distance_input, distance_output, distance_setpoint;
+double angle_input, angle_output, angle_setpoint, angle_RealSetpoint;
+
+double distance_kP = 1;            //****
+double distance_kI = 0;            //****
+double distance_kD = 0;            //****
+double angle_kP = 3.5;             //****
+double angle_kI = 0.0005;          //****
+double angle_kD = 4;               //****
+
+PID distance_PID  (&distance_input, &distance_output, &distance_setpoint,
+                   distance_kP, distance_kI, distance_kD, DIRECT);
+PID angle_PID     (&angle_input, &angle_output, &angle_RealSetpoint,
+                   angle_kP, angle_kI, angle_kD, DIRECT);
+
+
+
+//Basic setup steps
+void setup(){
+  Serial.begin(9600);
+  InitializeIMU();
+  left_thruster.attach(left_thruster_pin);
+  right_thruster.attach(right_thruster_pin);
+  distance_PID.SetMode(AUTOMATIC);
+  angle_PID.SetMode(AUTOMATIC);
+  distance_PID.SetOutputLimits(-300.00, 300.00);     //assuming max power is +-300
+  angle_PID.SetOutputLimits(-100.00, 100.00);
+  
+  distance_input = 0;                      //The distance input will always be 0
+  
+  distance_setpoint = 0;                   //****        changes based on cv and nav
+  angle_setpoint = 0;                      //****        changes based on cv and nav
+  delay(6000);
+}
+
+
+
+//Plugs IMU angle into PID and moves Thrusters
+void loop(){
+  IMU_x_angle();
+  Horizontal_Outputs();
+}
+
+
+
+//Turns IMU data collecting on
+void InitializeIMU(){
+  Wire.begin();
+  //Wire.beginTransmission(0x28);  //don't need for now but will need when we have > 1 sensor
+  if(!bno.begin()) {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  bno.setExtCrystalUse(true);
+}
+
+
+
+//Gets the X angle from the IMU
+void IMU_x_angle(){
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    angle_input = euler.x();
+}
+
+
+
+//Calculates PIDs and sends Outputs
+void Horizontal_Outputs(){
+  Path_Optimization();
+  
+  distance_PID.Compute();
+  angle_PID.Compute();
+  mixer();
+  
+  Serial.print("Left: ");
+  Serial.print(final_left_thruster_value);
+  Serial.print("-------");
+  Serial.print("Right: ");
+  Serial.println(final_right_thruster_value);
+  left_thruster.writeMicroseconds(final_left_thruster_value);
+  right_thruster.writeMicroseconds(final_right_thruster_value);
+}
+
+
+
+//Takes Error and Chooses which Direction to turn
+void Path_Optimization(){
+  double x = 180.00;
+  double y = -180.00;
+  double angle_error = angle_setpoint - angle_input;
+  if (angle_error > x){
+    angle_error = angle_error - 360;  }
+  else if (angle_error < y){
+    angle_error = angle_error + 360;  }
+    
+  angle_RealSetpoint = angle_error;
+  angle_input = 0.00;
+}
+
+
+
+//Takes PID outputs and makes pwm for Thrusters
+void mixer(){
+    final_left_thruster_value = 1500 + distance_output + angle_output;
+    final_right_thruster_value = 1500 + distance_output - angle_output;    //could also delete the - angle_output here and only increase other side:
+}                                                                          //.. would need an if statement to set negative angle_output to zero
---
+++ Magikarp_Code/Vertical_Thruster_Testing.ino
@@ -0,0 +1,231 @@
+/* Robosub 2019-2020 Vertical Thruster Testing
+   
+   Feature: IMU for Y & Z angles only
+            Output for Vertical Thrusters only
+
+   Revision History: 
+    Update (11/26/19):
+      Barometer kP value updated to 400
+
+   All values with "//****"  can be personalized
+*/
+
+
+
+//All librarys and IMU setup
+#include <PID_v1.h>
+#include <Wire.h>
+#include <Servo.h>
+#include "MS5837.h"
+#include <Adafruit_BNO055.h>
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+Adafruit_BNO055 bno = Adafruit_BNO055();
+MS5837 sensor;
+
+
+
+//Start thrusters as servos and set Pins**
+Servo front_right_thruster;
+Servo front_left_thruster;
+Servo back_left_thruster;
+Servo back_right_thruster;
+
+double final_front_right_thruster_value;
+double final_front_left_thruster_value;
+double final_back_left_thruster_value;
+double final_back_right_thruster_value;
+
+int front_right_thruster_pin = 5;           //****
+int front_left_thruster_pin = 6;            //****
+int back_left_thruster_pin = 9;             //****
+int back_right_thruster_pin = 7;            //****
+
+
+
+//Start all PIDs and set k values**
+double barometer_input, barometer_output, barometer_setpoint;
+double Y_angle_input, Y_angle_output, Y_angle_setpoint, Y_angle_RealSetpoint;
+double Z_angle_input, Z_angle_output, Z_angle_setpoint, Z_angle_RealSetpoint;
+
+double barometer_kP = 400;             //****
+double barometer_kI = 0;              //****
+double barometer_kD = 0;              //****
+double Y_angle_kP = 3.5;              //****
+double Y_angle_kI = 0.0005;           //****
+double Y_angle_kD = 4;                //****
+double Z_angle_kP = 3.5;              //****
+double Z_angle_kI = 0.0005;           //****
+double Z_angle_kD = 3.5;              //****
+
+PID barometer_PID  (&barometer_input, &barometer_output, &barometer_setpoint,
+                   barometer_kP, barometer_kI, barometer_kD, DIRECT);
+PID Y_angle_PID    (&Y_angle_input, &Y_angle_output, &Y_angle_RealSetpoint,
+                   Y_angle_kP, Y_angle_kI, Y_angle_kD, DIRECT);
+PID Z_angle_PID    (&Z_angle_input, &Z_angle_output, &Z_angle_RealSetpoint,
+                   Z_angle_kP, Z_angle_kI, Z_angle_kD, DIRECT);
+
+
+
+//Basic setup steps
+void setup(){
+  Serial.begin(9600);
+  InitializeIMU();
+  InitializeBarometer();
+  front_right_thruster.attach(front_right_thruster_pin);
+  front_left_thruster.attach(front_left_thruster_pin);
+  back_left_thruster.attach(back_left_thruster_pin);
+  back_right_thruster.attach(back_right_thruster_pin);
+  
+  barometer_PID.SetMode(AUTOMATIC);
+  Y_angle_PID.SetMode(AUTOMATIC);
+  Z_angle_PID.SetMode(AUTOMATIC);
+  
+  Y_angle_PID.SetOutputLimits(-100.00, 100.00);
+  Z_angle_PID.SetOutputLimits(-100.00, 100.00);
+  barometer_PID.SetOutputLimits(-300.00, 300.00);     //assuming max power is +-300
+  
+  barometer_setpoint = 0.15;          //****        changes based on cv and nav
+  Y_angle_setpoint = 0;               //****
+  Z_angle_setpoint = 0;               //****
+  delay(6000);                        //Thrusters need time to turn on or they dont spin
+}
+
+
+
+//Plugs IMU angle into PID and moves Thrusters
+void loop(){
+  IMU_YZ_angles();
+  Barometer_Reading();
+  Vertical_Outputs();
+}
+
+
+
+//Turns IMU data collecting on
+void InitializeIMU(){
+  //Wire.begin();       //Can use this if dealing with only 1 sensor
+  Wire.beginTransmission(0x28);
+  if(!bno.begin()) {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.println("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  bno.setExtCrystalUse(true);
+}
+
+
+
+//Turns Barometer data collecting on
+void InitializeBarometer(){
+  //Wire.begin();       //Can use this if dealing with only 1 sensor
+  Wire.beginTransmission(0x76);
+  while (!sensor.init()) {
+    Serial.println("Initilization failed");
+    delay(2000);
+  }
+  sensor.setModel(MS5837::MS5837_30BA);
+  sensor.setFluidDensity(997);      //(air ~ 1.23, freshwater ~ 997, seawater ~ 1029)
+}                                   //air is not accurate
+
+
+
+//Gets the Y and Z angles from the IMU
+void IMU_YZ_angles(){
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    Y_angle_input = euler.z();  // our y is roll
+    Z_angle_input = euler.y();  // our z is pitch
+}
+
+
+
+//Gets the Depth from the Barometer
+void Barometer_Reading(){
+  sensor.read();
+  barometer_input = sensor.depth();
+//  Serial.print("Depth: ");             //Use Depth in incompressible liquid only
+//  Serial.print(sensor.depth()); 
+//  Serial.println(" m");
+
+/*                                    //Don't need any of these for sub to run
+Serial.print("Pressure: "); 
+Serial.print(sensor.pressure()); 
+Serial.println(" mbar");
+
+Serial.print("Temperature: "); 
+Serial.print(sensor.temperature()); 
+Serial.println(" deg C");
+  
+Serial.print("Altitude: ");         //Use Altitude in air only (very not good data)
+Serial.print(sensor.altitude()); 
+Serial.println(" m above mean sea level");
+*/
+}
+
+
+
+//Calculates PIDs and sends Outputs
+void Vertical_Outputs(){
+  Path_Optimization();
+  barometer_PID.Compute();
+  Y_angle_PID.Compute();
+  Z_angle_PID.Compute();
+  Mixer();
+  Serial_Print_Thruster_Values();
+  
+  front_right_thruster.writeMicroseconds(final_front_right_thruster_value);
+  front_left_thruster.writeMicroseconds(final_front_left_thruster_value);
+  back_left_thruster.writeMicroseconds(final_back_left_thruster_value);
+  back_right_thruster.writeMicroseconds(final_back_right_thruster_value);
+}
+
+
+
+//Takes Error and Chooses which Direction to turn
+void Path_Optimization(){
+  double x = 180.00;
+  double y = -180.00;
+  double Y_angle_error = Y_angle_setpoint - Y_angle_input;
+  double Z_angle_error = Z_angle_setpoint - Z_angle_input;
+  
+  if (Y_angle_error > x){
+    Y_angle_error = Y_angle_error - 360;  }
+  else if (Y_angle_error < y){
+    Y_angle_error = Y_angle_error + 360;  }
+  
+  if (Z_angle_error > x){
+    Z_angle_error = Z_angle_error - 360;  }
+  else if (Z_angle_error < y){
+    Z_angle_error = Z_angle_error + 360;  }
+  
+  Y_angle_RealSetpoint = Y_angle_error;
+  Z_angle_RealSetpoint = Z_angle_error;
+  Y_angle_input = 0.00;
+  Z_angle_input = 0.00;
+}
+
+
+
+//Takes PID outputs and makes pwm for Thrusters
+void Mixer(){
+    final_front_right_thruster_value = 1500 + barometer_output + Y_angle_output + Z_angle_output;
+    final_front_left_thruster_value = 1500 + barometer_output - Y_angle_output + Z_angle_output;
+    final_back_left_thruster_value = 1500 + barometer_output - Y_angle_output - Z_angle_output;
+    final_back_right_thruster_value = 1500 + barometer_output + Y_angle_output - Z_angle_output;
+}
+
+
+
+//Print All Thrusters Values
+void Serial_Print_Thruster_Values(){
+  Serial.print("FrontRight: ");
+  Serial.print(final_front_right_thruster_value);
+  Serial.print("----");
+  Serial.print("FrontLeft: ");
+  Serial.print(final_front_left_thruster_value);
+  Serial.print("----");
+  Serial.print("BackLeft: ");
+  Serial.print(final_back_left_thruster_value);
+  Serial.print("----");
+  Serial.print("BackRight: ");
+  Serial.println(final_back_right_thruster_value);
+}
